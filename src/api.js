c3290e434294d4817d4b050a2cdf5d85ca8811e5
RyanKarakoc <kochy@hotmail.co.uk>
2023-04-17 16:58:21 +0100

    ticket 4 made amendmants to base url in api

--- src/api.js
+++ src/api.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 
+const gamesAPI = axios.create({
+  baseURL: "https://nc-games-project-v9v9.onrender.com/api",
+});
+
 export const fetchReviews = () => {
-  return axios
-    .get("https://nc-games-project-v9v9.onrender.com/api/reviews")
-    .then((response) => {
-      return response.data.reviews
-    });
+  return gamesAPI.get("/reviews").then((response) => {
+    return response.data.reviews;
+  });
 };
 
-
 export const fetchUsers = () => {
-  return axios
-  .get("https://nc-games-project-v9v9.onrender.com/api/users")
-  .then((response) => {
-    return response.data.users
+  return gamesAPI.get("/users").then((response) => {
+    return response.data.users;
   });
-}
+};
